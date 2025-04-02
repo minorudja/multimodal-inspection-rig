a3d3f7defd3b8061b4eb491e1e1b60bff46dbc62
minorudananjaya <115642409+minorudananjaya@users.noreply.github.com>
2025-04-02 15:46:11 +0100

    Create controlTurntable.ino
    Arduino code for controlling stepper motor turntable

---
+++ controlTurntable.ino
@@ -0,0 +1,61 @@
+/* Pin Definitions */ 
+// Stepper Motor Pins
+#define DIR_PIN 2
+#define STEP_PIN 3
+
+// Button Pin
+#define BUTTONPIN 4
+
+// Status LED Pin
+#define PIN_STATUS_LED 13 
+
+// Function Definitions
+void rotateTurntable(int angle, int speed);
+
+void setup() {
+    Serial.begin(9600);
+
+    // Stepper Motor Pins
+    pinMode(DIR_PIN, OUTPUT); 
+    pinMode(STEP_PIN, OUTPUT); 
+  
+    // Button Pin
+    pinMode(BUTTONPIN, INPUT);
+  
+}
+
+void loop() {
+    // turn status LED on
+    digitalWrite(PIN_STATUS_LED,  HIGH);
+   
+    if (digitalRead(BUTTONPIN) == HIGH){
+        rotateTurntable();    // start photoshooting
+    }
+    delay(2000); // delay for button debouncing
+}
+
+void rotateTurntable(int angle, float speed){
+    // Angle Check (Angle below 180 degrees)
+    if (abs(angle) > 180){
+        angle = int(angle/angle * 180);
+    }
+
+    // Speed Check (Must be between 0.01 and 1)
+    if (speed < 0.01){
+        speed = 0.01;
+    } else if (speed > 1){
+        speed = 1;
+    }
+
+    // Need to change once stepper motor characteristics known or after calibration
+    int steps = abs(angle)*(1/0.225);
+    float usDelay = (1/speed) * 70;
+
+    for (int i = 0; i < steps; i++){ 
+        digitalWrite(STEP_PIN, HIGH); 
+        delayMicroseconds(usDelay); 
+    
+        digitalWrite(STEP_PIN, LOW); 
+        delayMicroseconds(usDelay);
+    }
+}
